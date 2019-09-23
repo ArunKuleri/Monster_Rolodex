a56f6dd229b3ea4d3f5e78a2bd0453e089ab6695
Your Name <you@example.com>
2019-09-23 15:56:53 +0530

    Updates

---
+++ precache-manifest.fcebd0fb8d50eb6aa9fde0ec2caeef0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5eaa98e2292d147c11a8739dd3014b",
+    "url": "/Monster_Rolodex/index.html"
+  },
+  {
+    "revision": "7a3f2e900437bfba32d4",
+    "url": "/Monster_Rolodex/static/css/main.cab7eed5.chunk.css"
+  },
+  {
+    "revision": "a7581d0344af995cdff5",
+    "url": "/Monster_Rolodex/static/js/2.a1c35381.chunk.js"
+  },
+  {
+    "revision": "7a3f2e900437bfba32d4",
+    "url": "/Monster_Rolodex/static/js/main.c66596c4.chunk.js"
+  },
+  {
+    "revision": "639647c8836c0cb886f9",
+    "url": "/Monster_Rolodex/static/js/runtime~main.7e724871.js"
+  }
+]);
